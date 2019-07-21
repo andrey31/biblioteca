d8ce075b9b8dd91d1cfe3ca39cf1547aabc0d222
andrey31 <andreybm31@gmail.com>
2019-07-20 21:41:31 -0600

    Initial commit

---
+++ src/main/java/com/biblioteca/biblioteca/Main.java
@@ -0,0 +1,12 @@
+package com.biblioteca.biblioteca;
+
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
---
+++ src/main/java/com/biblioteca/biblioteca/modelos/Conexion.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.biblioteca.biblioteca.modelos;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conexion {
+
+    private Connection connection = null;
+    private String url = "";
+    private String server = "80.211.39.128:3310";
+    private String user = "m2";
+    private String pass = "metodologia2";
+    private String dataBase = "biblioteca_db";
+
+    public void connect() {
+
+        try {
+
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            url = "jdbc:mysql://" + server + "/" + dataBase;
+            connection = DriverManager.getConnection(url, user, pass);
+            System.out.println("Conected");
+
+        } catch (SQLException | ClassNotFoundException e) {
+
+            System.out.println("Error");
+            System.out.println(e);
+
+        }
+
+    }
+    
+    public Connection getConnection(){
+        return connection;
+    }
+
+    public Connection closeConnection(){
+        try {
+            connection.close();
+            System.out.println("close connection....ok");
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        connection = null;
+        return connection;
+    }
+
+}
