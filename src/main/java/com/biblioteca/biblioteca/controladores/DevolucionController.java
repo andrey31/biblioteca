6c8334a98054bdf2e8e9c4326bedfffe0739a096
rodri <rodri@DESKTOP-ATQTSJN>
2019-07-28 00:11:15 -0600

    Agregado update

--- src/main/java/com/biblioteca/biblioteca/controladores/DevolucionController.java
+++ src/main/java/com/biblioteca/biblioteca/controladores/DevolucionController.java
@@ -5,8 +5,12 @@ import com.biblioteca.biblioteca.modelos.entidades.Devoluciones;
 import com.biblioteca.biblioteca.vistas.JFDevoluciones;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.sql.Date;
 
 import java.sql.SQLException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import javax.swing.JOptionPane;
 import javax.swing.table.DefaultTableModel;
 
 /*
@@ -17,10 +21,13 @@ public class DevolucionController {
     private DevolucionesDAO devolucionesDAO;
     private JFDevoluciones jfDev;
     private Devoluciones dev;
+    private int idDev = 0;
+    private boolean actualizar = false;
 
     public DevolucionController() throws SQLException {
         this.jfDev = new JFDevoluciones();
-        datosPrestamo("2019-07-01", "4");
+        Paneles(true, false);
+        datosPrestamo("2019-07-01", "7");
         cargarDevolucion();
         jfDev.setVisible(true);
 
@@ -28,8 +35,17 @@ public class DevolucionController {
             @Override
             public void actionPerformed(ActionEvent e) {
                 try {
-                    ingresarDevolucion();
-                    cargarDevolucion();
+                    devolucionesDAO = new DevolucionesDAO();
+                    if (!devolucionesDAO.devoluciónExiste(Integer.parseInt(jfDev.lbIdPrestamo.getText()))) {
+
+                        ingresarDevolucion();
+                        cargarDevolucion();
+                        limpiarCampos();
+                    } else {
+                        limpiarCampos();
+                        JOptionPane.showMessageDialog(null, "Devolución ya ingresada");
+                    }
+
                 } catch (Exception ex) {
 
                 }
@@ -39,14 +55,40 @@ public class DevolucionController {
             @Override
             public void actionPerformed(ActionEvent e) {
                 try {
-
+                    if (actualizar == false) {
+                        cargarDatosUpdate();
+                    }
                 } catch (Exception ex) {
-
+                    JOptionPane.showMessageDialog(null, ex.getMessage());
+                }
+            }
+        });
+        this.jfDev.btnActualizar.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    if (actualizar) {
+                        updateDevolucion();
+                    }
+                } catch (Exception ex) {
+                    JOptionPane.showMessageDialog(null, ex.getMessage());
+                }
+            }
+        });
+        this.jfDev.btnCancelarUpdate.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    if (actualizar) {
+                        cancelarUpdate();
+                    }
+                } catch (Exception ex) {
                 }
             }
         });
     }
 
+    //Carga todos los registros en la tabla
     public void cargarDevolucion() throws SQLException {
         devolucionesDAO = new DevolucionesDAO();
         String[] columns = new String[]{"id", "Fecha propuesta", "Fecha devolución", "Multa", "Préstamo"};
@@ -68,6 +110,7 @@ public class DevolucionController {
 
     }
 
+    //Nuevo registro de devoluciones
     public void ingresarDevolucion() throws Exception {
         devolucionesDAO = new DevolucionesDAO();
 
@@ -77,13 +120,73 @@ public class DevolucionController {
         dev.setMulta(Double.parseDouble(this.jfDev.txtMulta.getText()));
 
         devolucionesDAO.saveDevolucion(dev);
+        limpiarCampos();
     }
 
+    //Carga los datos en los campos para hacer el update
+    public void cargarDatosUpdate() {
+        JOptionPane.showMessageDialog(null, "Hola");
+        if (this.jfDev.tbTabla.getSelectedRow() != -1) {
+
+            dev = new Devoluciones();
+            idDev = (int) this.jfDev.tbTabla.getModel().getValueAt(this.jfDev.tbTabla.getSelectedRow(), 0);
+            this.jfDev.txtFechaProp.setText((String) this.jfDev.tbTabla.getModel().getValueAt(this.jfDev.tbTabla.getSelectedRow(), 1));
+            this.jfDev.dtpFechaDev.setText((String) this.jfDev.tbTabla.getModel().getValueAt(this.jfDev.tbTabla.getSelectedRow(), 2));
+            this.jfDev.txtMulta.setText(this.jfDev.tbTabla.getModel().getValueAt(this.jfDev.tbTabla.getSelectedRow(), 3) + "");
+            this.jfDev.lbIdPrestamo.setText(this.jfDev.tbTabla.getModel().getValueAt(this.jfDev.tbTabla.getSelectedRow(), 4) + "");
+            actualizar = true;
+            jfDev.lbError.setText("");
+            Paneles(false, true);
+        }
+        if (this.jfDev.tbTabla.getSelectedRow() == -1) {
+            jfDev.lbError.setText("Elija la fila de la tabla que sea actualizar");
+        }
+
+    }
+
+    //Actualiza el registro
+    public void updateDevolucion() throws SQLException {
+        if (idDev != 0) {
+            dev.setFechaDevolucion(this.jfDev.dtpFechaDev.getText());
+            dev.setId(idDev);
+            dev.setMulta(Double.parseDouble(this.jfDev.txtMulta.getText()));
+
+            devolucionesDAO.updateDevolucion(dev);
+        }
+
+        idDev = 0;
+        actualizar = false;
+        limpiarCampos();
+        Paneles(true, false);
+    }
+
+    //Captura los datos del módulo de préstamo
     public void datosPrestamo(String fecha, String id) {
         this.jfDev.txtFechaProp.setText(fecha);
         this.jfDev.lbIdPrestamo.setText(id);
     }
-    /* public double calcularMulta() {
+
+    public void limpiarCampos() {
+        this.jfDev.dtpFechaDev.setText("");
+        this.jfDev.txtMulta.setText("0");
+        this.jfDev.lbIdPrestamo.setText("");
+        jfDev.lbError.setText("");
+    }
+
+    public void Paneles(boolean editar, boolean actualizar) {
+        jfDev.pnEditar.setVisible(editar);
+        jfDev.pnActualizar.setVisible(actualizar);
+        jfDev.tbTabla.enable(editar);
+
+    }
+
+    public void cancelarUpdate() throws SQLException {
+        Paneles(true, false);
+        limpiarCampos();
+
+    }
+
+    /*public double calcularMulta() {
         double multa = 0;
         try {
             SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
@@ -95,7 +198,7 @@ public class DevolucionController {
 
             //System.out.println("Hay " + dias + " dias de diferencia");
         } catch (ParseException ex) {
-            Logger.getLogger(DevolucionController.class.getName()).log(Level.SEVERE, null, ex);
+
         }
         return multa;
     }*/
