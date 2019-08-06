c92086da483853be66ff41dbe7652ca60a4f01c0
cesarmv99 <cesarmoya15@gmail.com>
2019-08-05 23:31:12 -0600

    Cambios minimos

--- src/main/java/com/biblioteca/biblioteca/vistas/JFBusqueda.java
+++ src/main/java/com/biblioteca/biblioteca/vistas/JFBusqueda.java
@@ -1,5 +1,7 @@
 package com.biblioteca.biblioteca.vistas;
 
+import java.awt.event.KeyListener;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
